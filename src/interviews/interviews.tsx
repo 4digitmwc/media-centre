1ae096eb5864bec71c49c82ce034ffdf84376d96
IndexError <lkycst@gmail.com>
2022-08-21 16:33:50 +0700

    add sample text for articles dashboard

---
+++ src/interviews/interviews.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react"
+import ReactMarkdown from 'react-markdown'
+import axios from 'axios'
+
+const Interview: React.FC = () => {
+    return <div>
+        This is the interview
+    </div>
+}
+
+export default Interview
---
+++ src/opinions/opinions.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react"
+import ReactMarkdown from 'react-markdown'
+import axios from 'axios'
+
+const Opinion: React.FC = () => {
+    return <div>
+        This is the opinions
+    </div>
+}
+
+export default Opinion
