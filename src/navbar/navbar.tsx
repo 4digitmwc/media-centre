83c0e0653f5a239cba18b632ed6111247fb98dde
IndexError <lkycst@gmail.com>
2022-08-21 16:22:00 +0700

    add navbar and dark theme

---
+++ src/navbar/navbar.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react"
+import ReactMarkdown from 'react-markdown'
+import axios from 'axios'
+
+import logo from "../logo.svg"
+import NavbarStyle from "./navbar.module.css"
+import { HashRouter, Link } from "react-router-dom"
+
+const NavBar: React.FC = () => {
+
+    
+
+    return <div className={NavbarStyle.navbar}>
+        <HashRouter>
+            <Link className={NavbarStyle.stories} to="/stories">Stories</Link>
+            <Link className={NavbarStyle.stories} to="/interviews">Interviews</Link>
+            <img className={NavbarStyle.logo} src={logo} alt="4dm2023-logo"/>
+            <Link className={NavbarStyle.stories} to="/opinions">Opinions</Link>
+            <a className={NavbarStyle.stories} href="https://4digitmwc.github.io/skillban-articles/">Skillbans</a>
+        </HashRouter>
+    </div>
+}
+
+export default NavBar
---
+++ src/templates/thumbnailbutton.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react"
+
+export interface IThumbnail {
+    text: string,
+    thumbnail_url: string,
+    size: {
+        width: number,
+        height: number
+    }
+}
+
+const ThumbnailButton: React.FC<IThumbnail> = ({text, thumbnail_url, size: {width, height}}) => {
+    return <div>
+
+    </div>
+}
+
+export default ThumbnailButton
