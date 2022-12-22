f257a6123efd285147aa74e467148f0437e0a986
DannyPX <damnoga@hotmail.com>
2022-12-22 14:46:42 +0100

    Add fetchArticles

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,54 @@ import Home from './pages/Home/Home';
 import Stories from './pages/Stories/Stories';
 import Interviews from './pages/Interviews/Interviews';
 import Opinions from './pages/Opinions/Opinions';
+import ARTICLES, { IArticles } from './articles/Articles';
+import Article from './components/Article/Article';
 
-class App extends React.Component {
+const default_url = process.env.REACT_APP_MEDIA_CENTRE_DEFAULT_URL;
+
+interface IState {
+  articles_routes: JSX.Element[];
+}
+
+class App extends React.Component<any, IState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      articles_routes: this.fetchArticles(ARTICLES),
+    };
+  }
+
+  fetchArticles(articles: IArticles) {
+    let routes = [];
+    for (const [category, articles_] of Object.entries(articles)) {
+      for (const article of articles_) {
+        const article_path = `/${category}/${article}`;
+        const article_url = default_url + `/${category}/${article}`;
+        routes.push(
+          <Route
+            path={article_path}
+            element={<Article article_url={article_url} />}
+          />
+        );
+      }
+    }
+
+    return [
+      ...routes,
+      <Route path={RouterPathEnum.HOME} element={<Home />} />,
+      <Route path={RouterPathEnum.STORIES} element={<Stories />} />,
+      <Route path={RouterPathEnum.INTERVIEWS} element={<Interviews />} />,
+      <Route path={RouterPathEnum.OPINIONS} element={<Opinions />} />,
+    ];
+  }
+
   render() {
     return (
       <div className={styles.App}>
         <BrowserRouter basename={process.env.PUBLIC_URL}>
           <NavBar />
-          <Routes>
-            <Route path={RouterPathEnum.HOME} element={<Home />} />
-            <Route path={RouterPathEnum.STORIES} element={<Stories />} />
-            <Route path={RouterPathEnum.INTERVIEWS} element={<Interviews />} />
-            <Route path={RouterPathEnum.OPINIONS} element={<Opinions />} />
-          </Routes>
+          <Routes>{this.state.articles_routes}</Routes>
         </BrowserRouter>
       </div>
     );
